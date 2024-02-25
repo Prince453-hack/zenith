2e5681ad4d75f75fc4b4ef406184a74ffc7f5b37
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-02-25 14:31:04 +0530

    loading state

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Canvas } from "./_components/canvas";
+import { Room } from "@/components/room";
+import { Loading } from "./_components/canvas-loading";
 
 interface Props {
   params: {
@@ -8,7 +10,11 @@ interface Props {
 }
 
 const Page = ({ params }: Props) => {
-  return <Canvas boardId={params.boardId} />;
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 };
 
 export default Page;
--- components/room.tsx
+++ components/room.tsx
@@ -2,16 +2,18 @@
 
 import { RoomProvider } from "@/liveblocks.config";
 import { ClientSideSuspense } from "@liveblocks/react";
+import { ReactNode } from "react";
 
 interface Props {
   children: React.ReactNode;
   roomId: string;
+  fallback: NonNullable<ReactNode> | null;
 }
 
-export const Room = ({ children, roomId }: Props) => {
+export const Room = ({ children, roomId, fallback }: Props) => {
   return (
     <RoomProvider id={roomId} initialPresence={{}}>
-      <ClientSideSuspense fallback={<div>Loading</div>}>
+      <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>
     </RoomProvider>
