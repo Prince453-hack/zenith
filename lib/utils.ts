0ec0469416ff09bd3de5939b8ca5d4f362507419
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-02-25 18:13:19 +0530

    participants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = [
+  "#EE4266",
+  "#FBA834",
+  "#0C359E",
+  "#344955",
+  "#50C4ED",
+  "#9BCF53",
+];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
