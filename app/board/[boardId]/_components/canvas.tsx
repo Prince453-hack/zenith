1008dd50d9fec00e70b4ab80366407e5cc2b085e
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-02-26 23:06:34 +0530

    pointer events

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { CanvasMode, CanvasState } from "@/types/canvas";
-import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import {
+  useCanRedo,
+  useCanUndo,
+  useHistory,
+  useMutation,
+} from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvas } from "@/lib/utils";
 
 interface Props {
   boardId: string;
@@ -18,6 +25,28 @@ export const Canvas = ({ boardId }: Props) => {
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+
+  const onPointerMove = useMutation(
+    ({ setMyPresence }, e: React.PointerEvent) => {
+      e.preventDefault();
+
+      const current = pointerEventToCanvas(e, camera);
+      setMyPresence({ cursor: current });
+    },
+    []
+  );
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({ cursor: null });
+  }, []);
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -31,6 +60,21 @@ export const Canvas = ({ boardId }: Props) => {
         undo={history.undo}
         redo={history.redo}
       />
+
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g
+          style={{
+            transform: `translate(${camera.x}px, ${camera.y}px)`,
+          }}
+        >
+          <CursorsPresence />
+        </g>
+      </svg>
     </main>
   );
 };
