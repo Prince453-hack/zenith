5568e14eccce73665e71f465af6bd498d7dfe379
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-02-27 21:06:35 +0530

    selection box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -25,6 +25,7 @@ import { connectionIdColor, pointerEventToCanvas } from "@/lib/utils";
 import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
+import { SelectionBox } from "./selection-box";
 
 const MAX_LAYERS = 100;
 
@@ -186,6 +187,7 @@ export const Canvas = ({ boardId }: Props) => {
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox onResizeHandlerPointerDown={() => {}} />
           <CursorsPresence />
         </g>
       </svg>
