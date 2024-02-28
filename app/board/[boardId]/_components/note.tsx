eed1738eddf63f2d6f339a5f8a9f71f1c19c7ca1
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-02-28 22:09:03 +0530

    sticky-notes

---
+++ app/board/[boardId]/_components/note.tsx
@@ -0,0 +1,69 @@
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+import { Kalam } from "next/font/google";
+import { NoteLayer } from "@/types/canvas";
+import { cn, colorToCss, getContrastingTextColor } from "@/lib/utils";
+import { useMutation } from "@/liveblocks.config";
+
+const font = Kalam({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const calculateFontSize = (width: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.15;
+
+  const fontSizeBasedOnHeight = height * scaleFactor;
+  const fontSizeBasedOnWidth = width * scaleFactor;
+
+  return Math.min(fontSizeBasedOnHeight, fontSizeBasedOnWidth, maxFontSize);
+};
+
+interface Props {
+  id: string;
+  layer: NoteLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Note = ({ id, layer, onPointerDown, selectionColor }: Props) => {
+  const { x, y, fill, height, width, value } = layer;
+
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  };
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+        backgroundColor: fill ? colorToCss(fill) : "#000",
+      }}
+      className="drop-shadow-xl shadow-md"
+    >
+      <ContentEditable
+        html={value || "Text"}
+        onChange={handleContentChange}
+        className={cn(
+          "h-full w-full flex items-center justify-center text-center outline-none",
+          font.className
+        )}
+        style={{
+          fontSize: calculateFontSize(width, height),
+          color: fill ? getContrastingTextColor(fill) : "#000",
+        }}
+      />
+    </foreignObject>
+  );
+};
